41efa986aeb5bf6068c3c7a30a030655a0e9ebfb
Jordan Last <jordan.michael.last@gmail.com>
2020-01-31 23:53:25 -0700

    Add geth origin

--- src/graphql/graphql.ts
+++ src/graphql/graphql.ts
@@ -2,7 +2,7 @@ import { GQLResult } from "../..";
 import * as fetch from 'node-fetch';
 
 export async function gqlRequest(query: string, variables?: {}): Promise<GQLResult> {
-    const response = await fetch('http://localhost:8547/graphql', {
+    const response = await fetch(`${process.env.QUERY_ETHEREUM_GETH_GRAPHQL_ORIGIN}/graphql`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
