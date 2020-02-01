c7b31e0a2cad6ac54e849ca215b423e48e647853
Jordan Last <jordan.michael.last@gmail.com>
2020-01-31 23:50:03 -0700

    add port env variable

--- src/postgres/postgres.ts
+++ src/postgres/postgres.ts
@@ -3,7 +3,7 @@ import { Pool } from 'pg';
 export const postgres = new Pool({
     database: 'query_ethereum',
     host: process.env.QUERY_ETHEREUM_DB_HOST,
-    port: 5433,
+    port: parseInt(process.env.QUERY_ETHEREUM_DB_PORT || ''),
     user: 'postgres',
     password: process.env.QUERY_ETHEREUM_DB_PASSWORD,
     ssl: false
