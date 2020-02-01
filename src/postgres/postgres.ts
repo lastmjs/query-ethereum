f19c102afdf6141ae3244bd4ecec9bf52c4c6da5
Jordan Last <jordan.michael.last@gmail.com>
2020-01-31 23:41:37 -0700

    add db password and host environment variables

--- src/postgres/postgres.ts
+++ src/postgres/postgres.ts
@@ -1,10 +1,8 @@
 import { Pool } from 'pg';
 
-// -e POSTGRES_PASSWORD=password
-
 export const postgres = new Pool({
     database: 'query_ethereum',
-    host: 'localhost',
+    host: process.env.QUERY_ETHEREUM_DB_HOST,
     port: 5433,
     user: 'postgres',
     password: process.env.QUERY_ETHEREUM_DB_PASSWORD,
